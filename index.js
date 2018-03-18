d152e953004b1208f64e64b76bf456ce2649f6e9
dhrubesh <dhrubesh97@gmail.com>
2018-03-18 20:54:25 +0530

    integrated redux
    Signed-off-by: dhrubesh <dhrubesh97@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-import { AppRegistry } from 'react-native';
+import React, { Component } from "react";
+import { 
+AppRegistry,
+StyleSheet,
+  Text,
+    View
+    } from 'react-native';
 import App from './app/index.js';
+export default class Client extends Component {
+  render() {
+      return (
+            <View style={styles.container}>
+	            <App {...this.props}/>
+	     </View>
+	     );
+	    }
+}
 
-AppRegistry.registerComponent('client', () => App);
+const styles = StyleSheet.create({
+		  container: {
+			     flex: 1
+		            },
+	           welcome: {
+			      fontSize: 20,
+    		              textAlign: 'center',
+			      margin: 10,
+	   	            },
+	 	   instructions: {
+			      textAlign: 'center',
+	    	              color: '#333333',
+			      marginBottom: 5,
+			        },
+			});
+AppRegistry.registerComponent('client', () => Client);
